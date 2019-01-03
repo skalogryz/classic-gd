e2daf9c07ce737a3e72456ee0a12bb9d7fbba9e0
Dmitry Boyarintsev <skalogryz.lists@gmail.com>
2019-01-03 16:56:12 -0500

    cookies and cream - now can login

--- src/webcall.php
+++ src/webcall.php
@@ -4,36 +4,86 @@
   require_once("gamedevru.php");
   require_once("gamedevruold.php");
 
-function callHttp($url)
+function callHttp($url, $method, $baseurl)
 {
-/*  $r = new HttpRequest($url, HttpRequest::METH_GET);
-  try {
-    return $r->send()->getBody();
-  } catch (Exception $x)
-  {
-    echo $x;
+  $method = strtoupper($method);
+  $cookie = "";
+  foreach ($_COOKIE as $nm=>$vl) $cookie .= $nm.'='.$vl.";";
+  if ($cookie!="") $cookie = "Cookie: ".$cookie;
+
+  $opts = array(
+  'http'=>array(
+    'method'=>$method,
+    'header'=>$cookie 
+    )
+  );
+
+  $ispost = $method=='POST';
+
+  if ($ispost) {
+    $opts['http']['content']=http_build_query($_POST);
+    $hdr=$opts['http']['header'];
+    if ($hdr!="") $hdr.="\r\n";
+    $hdr.="Content-type: application/x-www-form-urlencoded\r\n";  
+    $opts['http']['header'] = $hdr;
   }
-*/
-  return file_get_contents($url);
-//  return http_get($url);
+
+  $context = stream_context_create($opts);
+  $res = file_get_contents($url, false, $context);
+
+  if ($ispost) {
+    $isredirect = false;
+
+    foreach($http_response_header as $h) {
+      if (!(strpos($h ,"Set-Cookie:")===false)) {
+        $s = str_replace("Set-Cookie:", "", $h);
+        $s = trim($s);
+        $arr = explode(";",$s,);
+        foreach($arr as $pair) {
+          $v = explode("=",trim($pair));
+          $nm = ""; $vl ="";
+          if (sizeof($v)<=1) $nm = $v[0]; 
+          else {
+            $nm = $v[0];
+            $vl = $v[1];
+          }
+          $unm = strtoupper($nm);
+          if (($unm == "EXPIRES") || ($unm == "PATH")|| ($unm == "SECURE")|| ($unm == "HTTPONLY")) continue; //todo: need to forward!
+
+          $parts = parse_url($baseurl);
+          setcookie($nm, $vl,  time()+60*60*24*365*2, $parts["path"], $parts["host"]); // todo: the cookie comes first, and then the rest of information
+          break;
+        }
+      } else if (!(strpos($h ,"Location:")===false)) {
+        $isredirect = true;
+      }
+    }
+    if ($isredirect) {
+      //todo: rebase
+      header("Location: ".$baseurl, true, 302); 
+      die(); // redirect and bail-out
+    }
+  }
+
+  return $res;
 }
-  //echo "hello world\r\n";
-  //print_r ($_GET);
-  //print_r ($_SERVER);
+
   $g_truesite = "https://gamedev.ru/";
   $g_proxypath = "/gamedev.ru";
-  //print_r ($_GET);
-  //print_r ($_SERVER);
-  $url = $_SERVER["REQUEST_URI"];
-  $url = substr_replace($url, "", 0, strlen($g_proxypath));
-  $url = $g_truesite.$url;
-  //echo "will call: $url\r\n";
 
-  $page = callHttp($url);
-  if (substr($_SERVER["SCRIPT_URL"],-3) == '.js') {
-    echo $page;
-    die;
-  } 
+  $basepath = $_SERVER["REQUEST_SCHEME"]."://".$_SERVER["HTTP_HOST"].$g_proxypath;
+
+  $url = $_SERVER["REQUEST_URI"];
+  // todo: cleanup the mess! :(
+  if ($_SERVER["REQUEST_METHOD"] != 'POST') {
+    $url = substr_replace($url, "", 0, strlen($g_proxypath));
+    $url = $g_truesite.$url;
+  } else {
+    if (strpos($url, "/")==0) $url=substr($url, 1);
+    $url = $g_truesite.$url;
+  }
+
+  $page = callHttp($url, $_SERVER["REQUEST_METHOD"], $basepath);
 
   $mode = "";
   if (strpos($_SERVER["SCRIPT_URL"], '/forum')) 
@@ -48,13 +98,10 @@ function callHttp($url)
   
   $xpath = new DOMXpath($doc);
   $gd = new GameDev();
-  $basepath = $_SERVER["REQUEST_SCHEME"]."://".$_SERVER["HTTP_HOST"].$g_proxypath;
   $gd->basepath = $basepath."/"; // this is used NOT to create a stand-alone host, but rather a directory in existing one
   
   GatherSite($xpath, $gd, $mode);
   $gd->rebase($basepath);
   OutputSite($gd, $mode);
-
-//  if /gamedev.ru/flame/forum/
   
 ?>
